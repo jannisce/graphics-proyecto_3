7f13751efffbfce64819aa21d5ac58f549f277d9
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2023-11-20 00:45:37 -0600

    refactor:cube.cpp

--- src/cube.cpp
+++ src/cube.cpp
@@ -13,11 +13,11 @@ Intersect Cube::rayIntersect(const glm::vec3& rayOrigin, const glm::vec3& rayDir
     glm::vec3 tmin = glm::min(t0, t1);
     glm::vec3 tmax = glm::max(t0, t1);
 
-    float tmin_final = glm::max(glm::max(tmin.x, tmin.y), tmin.z);
-    float tmax_final = glm::min(glm::min(tmax.x, tmax.y), tmax.z);
+    float tmin_final = glm::max(tmin.x, glm::max(tmin.y, tmin.z));
+    float tmax_final = glm::min(tmax.x, glm::min(tmax.y, tmax.z));
 
-    if (tmin_final > tmax_final || tmax_final < 0) {
-        return Intersect{false};
+    if (tmin_final > tmax_final) {
+        return Intersect{false}; // No intersection
     }
 
     glm::vec3 point = rayOrigin + tmin_final * rayDirection;
@@ -29,8 +29,8 @@ Intersect Cube::rayIntersect(const glm::vec3& rayOrigin, const glm::vec3& rayDir
 // Helper function to calculate normal based on the intersection point
 glm::vec3 Cube::calculateNormal(const glm::vec3& point) const {
     glm::vec3 normal = glm::vec3(0.0f);
+    const float bias = 1e-4; // Small bias to handle numerical precision issues
 
-    float bias = 1e-4; // Small bias to handle numerical precision issues
     if (std::fabs(point.x - min.x) < bias) normal.x = -1.0f;
     else if (std::fabs(point.x - max.x) < bias) normal.x = 1.0f;
     else if (std::fabs(point.y - min.y) < bias) normal.y = -1.0f;
